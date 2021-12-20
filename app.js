cf7356b44594b431a60305ed5a945feda40aa243
Alexey Akulich <loki1987by@yandex.ru>
2021-12-20 09:06:46 +0300

    fix: buggfix

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { TEXT, PIC_ARRAY } from "./consts.js";
+const BODY = document.querySelector("body");
 const body = document.querySelector(".karaoke");
 const gallery = document.querySelector(".img-container");
 const button = document.getElementById("play");
@@ -23,6 +24,26 @@ const createElement = (tagName, params, container, text) => {
 
   return element;
 };
+
+function launchFullScreen() {
+  if (BODY.requestFullScreen) {
+    BODY.requestFullScreen();
+  } else if (BODY.mozRequestFullScreen) {
+    BODY.mozRequestFullScreen();
+  } else if (BODY.webkitRequestFullScreen) {
+    BODY.webkitRequestFullScreen();
+  }
+}
+
+function cancelFullscreen() {
+  if (document.cancelFullScreen) {
+    document.cancelFullScreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitCancelFullScreen) {
+    document.webkitCancelFullScreen();
+  }
+}
 
 function textPainter(time, selector) {
   const letterTime = time / selector.children.length;
@@ -62,31 +83,31 @@ function karaoke(waiting) {
   setTimeout(() => {
     body.classList.remove('hidden')
     gallery.classList.remove('img-container_big')
-    createSongElement(3110, TEXT[0]);
+    createSongElement(3050, TEXT[0]);
     setTimeout(() => {
-      createSongElement(3110, TEXT[1]);
-    }, 3110 * 4 + 900);
+      createSongElement(3050, TEXT[1]);
+    }, 3050 * 4 + 950);
     /* припев */
     setTimeout(() => {
       createSongElement(3300, TEXT[2]);
-    }, 3110 * 8 + 4400);
+    }, 3050 * 8 + 4400);
     setTimeout(() => {
       createSongElement(3300, TEXT[2]);
-    }, 3110 * 8 + 3300 * 4 + 4400);
+    }, 3050 * 8 + 3300 * 4 + 4500);
     /* куплет 2 */
     setTimeout(() => {
       createSongElement(3110, TEXT[3]);
-    }, 3110 * 8 + 3300 * 8 + 5200);
+    }, 3050 * 8 + 3300 * 8 + 5600);
     setTimeout(() => {
       createSongElement(3110, TEXT[4]);
-    }, 3110 * 12 + 3300 * 8 + 6000);
+    }, 3050 * 12 + 3300 * 8 + 6400);
     setTimeout(() => {
       /* припев */
       createSongElement(3300, TEXT[5]);
-    }, 3110 * 16 + 3300 * 8 + 6500);
+    }, 3050 * 16 + 3300 * 8 + 6900);
     setTimeout(() => {
       createSongElement(3300, TEXT[5]);
-    }, 3110 * 16 + 3300 * 12 + 6500);
+    }, 3050 * 16 + 3300 * 12 + 6900);
   }, waiting);
 }
 
@@ -96,7 +117,13 @@ function playAudio() {
     const oldImage = document.querySelector('.image')
     oldImage.remove()
     button.classList.remove('hidden')
-    createElement('img', {class: 'image', alt: 'img', src: PIC_ARRAY[39]}, gallery)
+    cancelFullscreen()
+    const oldDiv = document.querySelector(".text-container");
+    if(oldDiv) {
+    oldDiv.remove();
+    }
+    gallery.classList.add('img-container_big')
+    createElement('img', {class: 'image image_big', alt: 'img', src: PIC_ARRAY[39]}, gallery)
   }, audio.duration * 1000);
 }
 
@@ -118,6 +145,7 @@ function gallerySwitcher() {
 }
 
 function startSong() {
+  launchFullScreen()
   playAudio();
   karaoke(25000);
   gallerySwitcher()
